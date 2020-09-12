e52c6080be63cf35ac2652bba5386845ac251801
4forces <toh.tingji@gmail.com>
2020-09-12 08:20:50 +0000

    commit 4

--- readcsv.py
+++ readcsv.py
@@ -2,7 +2,7 @@ import csv
 
 filepointer = open('test.csv')
 csvfile = csv.reader(filepointer, delimiter=",")
-next(csvfile) #skip the header (first) row
+next(csvfile) #skip the first (header) row
 for row in csvfile:
     print("ISBN:", row[0])
     print("Title:", row[1])
